ad8942b2b9e67cbbe289e3de8973e7224e66e81d
wgreco13 <wgreco13@gmail.com>
2022-05-16 11:27:26 -0400

    Added temporary comment change

--- src/components/containers/SideBar.jsx
+++ src/components/containers/SideBar.jsx
@@ -4,17 +4,10 @@ import PropButton from '../PropButton';
 
 const SideBar = ({ savedProps, currentProp, setCurrentProp }) => {
   //add stateful property to each prop drop
-<<<<<<< HEAD
-  console.log('saved', savedProps);
-  const sidePropElement = savedProps.map((element) => {
-    <PropDrop
-      savedProps={savedProps}
-=======
   const sidePropElement = savedProps.map((property) => {
     <PropButton
       propName={property.propName}
       property={property}
->>>>>>> dev
       currentProp={currentProp}
       //for editing purposes? or would we need setSavedProps?
       setCurrentProp={setCurrentProp}
